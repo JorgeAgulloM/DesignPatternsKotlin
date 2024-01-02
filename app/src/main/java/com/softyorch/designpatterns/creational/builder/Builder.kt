ae1a9ebfaea3cd0eb6604f2a7f48975d8514d227
Jorge1 <agullojorge@gmail.com>
2024-01-02 20:10:46 +0100

    (INIT) Builder Pattern

---
+++ app/src/main/java/com/softyorch/designpatterns/creational/builder/Builder.kt
@@ -0,0 +1,23 @@
+package com.softyorch.designpatterns.creational.builder
+
+/**
+ * *******************************************
+ * Problem
+ * *******************************************
+ */
+
+data class Hero(
+    val name:String?,
+    val age:Int?,
+    val description:Int?,
+    val power:String?,
+    val weakness:String?,
+    val isAvenger: Boolean?,
+    val isAlive: Boolean?,
+    val isVillain: Boolean?,
+)
+
+
+fun example() {
+    val nerdHero = Hero("SuperYorch", null, null, "KOTLIN", "FLUTTER", null, null, null)
+}
