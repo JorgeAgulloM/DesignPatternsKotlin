ae3033eab39bc33132cabfe23d0cd058d0d2d88e
Jorge1 <agullojorge@gmail.com>
2024-01-02 20:37:08 +0100

    (Builder Pattern) All methods returned your self interface

--- app/src/main/java/com/softyorch/designpatterns/creational/builder/Builder.kt
+++ app/src/main/java/com/softyorch/designpatterns/creational/builder/Builder.kt
@@ -1,12 +1,13 @@
 package com.softyorch.designpatterns.creational.builder
 
 interface Builder {
-    fun setName(name: String)
-    fun setAge(age: Int)
-    fun setDescription(description: String)
-    fun setPower(power: String)
-    fun setWeakness(weakness: String)
-    fun setIsAvenger(isAvenger: Boolean)
-    fun setIsAlive(isAlive: Boolean)
-    fun setIsVillain(isVillain: Boolean)
+    fun setName(name: String): Builder
+    fun setAge(age: Int): Builder
+    fun setDescription(description: String): Builder
+    fun setPower(power: String): Builder
+    fun setWeakness(weakness: String): Builder
+    fun setIsAvenger(isAvenger: Boolean): Builder
+    fun setIsAlive(isAlive: Boolean): Builder
+    fun setIsVillain(isVillain: Boolean): Builder
+    fun build(): Hero
 }
