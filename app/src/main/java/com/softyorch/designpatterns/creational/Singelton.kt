bbcb7b3b3a9dd61d34e2758d77916ff3ec451878
Jorge1 <agullojorge@gmail.com>
2024-01-02 20:00:13 +0100

    (Easier) Singleton Pattern

--- app/src/main/java/com/softyorch/designpatterns/creational/Singelton.kt
+++ app/src/main/java/com/softyorch/designpatterns/creational/Singelton.kt
@@ -38,20 +38,35 @@ class Poo() {
 
 // private constructor block new creations
 class Config2 private constructor() {
-    var name:String = ""
+    var name: String = ""
 
     companion object {
-        val instance:Config2 by lazy { Config2() }
+        val instance: Config2 by lazy { Config2() }
     }
 }
+
+/**
+ * *******************************************
+ * Solution two. Easier to use
+ * *******************************************
+ */
+
+object Config3 {
+    var name: String = ""
+}
+
 
 fun main() {
     val example1: Config2 = Config2.instance
     val example2: Config2 = Config2.instance
 
     example1.name = "Jorge"
+    print(example2.name) // print -> "Jorge"
 
-    print(example2.name) // print -> "Jorge
+    print("\n")
+
+    Config3.name = "Yorch"
+    print(Config3.name) // print -> "Yorch"
 }
 
 
