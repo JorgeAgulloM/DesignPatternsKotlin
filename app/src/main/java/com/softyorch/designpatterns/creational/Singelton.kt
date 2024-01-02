11354fe9b3373d4a953bf8e46b914b10a479b01c
Jorge1 <agullojorge@gmail.com>
2024-01-02 19:55:51 +0100

    (SOLUTION) Singleton Pattern

--- app/src/main/java/com/softyorch/designpatterns/creational/Singelton.kt
+++ app/src/main/java/com/softyorch/designpatterns/creational/Singelton.kt
@@ -2,7 +2,69 @@ package com.softyorch.designpatterns.creational
 
 /**
  * *******************************************
- * Problema
+ * Problem
  * *******************************************
  */
+
+class Config() {
+    var name: String = ""
+
+    //method
+    fun exampleMethod() {}
+}
+
+class Foo() {
+    private val config = Config()
+
+    fun example() {
+        config.name = "Jorge"
+        config.exampleMethod()
+    }
+}
+
+class Poo() {
+
+    fun example2() {
+        val config = Config()
+        print(config.name) // print -> ""
+    }
+}
+
+/**
+ * *******************************************
+ * Solution
+ * *******************************************
+ */
+
+// private constructor block new creations
+class Config2 private constructor() {
+    var name:String = ""
+
+    companion object {
+        val instance:Config2 by lazy { Config2() }
+    }
+}
+
+fun main() {
+    val example1: Config2 = Config2.instance
+    val example2: Config2 = Config2.instance
+
+    example1.name = "Jorge"
+
+    print(example2.name) // print -> "Jorge
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
