c69d9faec26da5273f578d9fd6a38a1fa71dbf9e
Jorge1 <agullojorge@gmail.com>
2024-01-02 20:43:40 +0100

    (Builder Pattern) Manager created with examples

---
+++ app/src/main/java/com/softyorch/designpatterns/creational/builder/MarvelManager.kt
@@ -0,0 +1,26 @@
+package com.softyorch.designpatterns.creational.builder
+
+class MarvelManager {
+
+    fun createSuperYorchHero(superHeroBuilder: SuperHeroBuilder): Hero {
+        return superHeroBuilder
+            .setName("SuperYorch")
+            .setAge(41)
+            .setIsAlive(true)
+            .setDescription("Cream")
+            .setIsVillain(false)
+            .setPower("Kotlin")
+            .setWeakness("Flutter")
+            .build()
+    }
+
+    fun createSuperYorchStrong(superHeroBuilder: SuperHeroBuilder): Hero {
+        return superHeroBuilder
+            .setName("SuperYorch")
+            .setAge(24)
+            .setDescription("The best Yorch")
+            .setPower("Kotlin")
+            .build()
+    }
+
+}
