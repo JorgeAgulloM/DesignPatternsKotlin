ff9e9e197a6b488aa04a4c9f3d937c1b4a75cea4
Jorge1 <agullojorge@gmail.com>
2024-01-04 20:34:12 +0100

    [Prototype] Refactored

--- app/src/main/java/com/softyorch/designpatterns/creational/pototype/PrototypePattern.kt
+++ app/src/main/java/com/softyorch/designpatterns/creational/pototype/PrototypePattern.kt
@@ -8,12 +8,10 @@ fun main() {
     val cloneSquare: IPrototype = originalSquare.clone()
 
     val clonedTriangleIsometric: IsometricShapes = cloneTriangle.getIsometricShape()
-
-    val clonedSquareIsometric: IsometricShapes = cloneSquare.getIsometricShape()
-
     println("Original: ${originalTriangle.getIsometricShape()}, Lados: ${originalTriangle.getIsometricShape().sides}")
     println("Clonado: $clonedTriangleIsometric, Lados: ${clonedTriangleIsometric.sides}")
 
+    val clonedSquareIsometric: IsometricShapes = cloneSquare.getIsometricShape()
     println("Original: ${originalSquare.getIsometricShape()}, Lados: ${originalSquare.getIsometricShape().sides}")
     println("Clonado: $clonedSquareIsometric, Lados: ${clonedSquareIsometric.sides}")
 
