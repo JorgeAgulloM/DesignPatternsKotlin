d38ed931b2883779ca1d1f496fe6d13e355d88f8
Jorge1 <agullojorge@gmail.com>
2024-01-09 19:49:56 +0100

    [COMMAND] Created primary class

---
+++ app/src/main/java/com/softyorch/designpatterns/behaviour/command/Light.kt
@@ -0,0 +1,11 @@
+package com.softyorch.designpatterns.behaviour.command
+
+class Light {
+    fun turnOn() {
+        println("Encender luces")
+    }
+
+    fun turnOff() {
+        println("Apagar luces")
+    }
+}
